bbb8d514e05e6c7e74ac285b50f9699cbe1763c6
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2020-08-13 00:00:48 +0300

    Delaying Search Input

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '9963f7f3',
-            s: 'avengers'
+            s: searchTerm
         }
     });
 
     console.log(response.data);
 };
+
+const input = document.querySelector('input');
+
+let timeoutId;
+const onInput = event => {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        fetchData(event.target.value);
+    }, 500)
+};
+
+input.addEventListener('input', onInput);
